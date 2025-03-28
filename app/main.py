f237dd46e7ed76ff07a693394e4dbe05f846c08b
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-03-28 12:53:39 +0200

    main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
 
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        self.wife = None
+        self.husband = None
+        Person.people[name] = self
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    # First pass: create all Person instances
+    persons = [Person(person['name'], person['age']) for person in people]
+
+    # Second pass: assign relationships
+    for person in people:
+        person_instance = Person.people[person['name']]
+        if 'wife' in person and person['wife']:
+            person_instance.wife = Person.people[person['wife']]
+        elif 'husband' in person and person['husband']:
+            person_instance.husband = Person.people[person['husband']]
+
+    return persons
