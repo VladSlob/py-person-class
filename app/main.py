2b5ba73cbf0ca33459c7ba46c49013f2f4affe8a
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-03 15:24:49 +0300

    main.py

--- app/main.py
+++ app/main.py
@@ -1,24 +1,22 @@
 class Person:
     people = {}
 
-    def __init__(self, name: str, age: int) -> None:  # Added return type annotation
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.wife = None
-        self.husband = None
         Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:  # Ensured two blank lines before function
-    # First pass: create all Person instances
-    persons = [Person(person["name"], person["age"]) for person in people]  # Replaced single quotes
+def create_person_list(people: list) -> list:
+    persons_list = [Person(person["name"], person["age"]) for person in people]
+    for number in range(len(people)):
+        if people[number].get("husband"):
+            persons_list[number].husband = (
+                persons_list[number].people[people[number]["husband"]]
+            )
+        elif people[number].get("wife"):
+            persons_list[number].wife = (
+                persons_list[number].people[people[number]["wife"]]
+            )
 
-    # Second pass: assign relationships
-    for person in people:
-        person_instance = Person.people[person["name"]]  # Replaced single quotes
-        if "wife" in person and person["wife"]:  # Replaced single quotes
-            person_instance.wife = Person.people[person["wife"]]  # Replaced single quotes
-        elif "husband" in person and person["husband"]:  # Replaced single quotes
-            person_instance.husband = Person.people[person["husband"]]  # Replaced single quotes
-
-    return persons
+    return persons_list
